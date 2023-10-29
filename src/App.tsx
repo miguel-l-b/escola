0651f6d4f80d3766e7113e2720f2e6e8ca7e0102
miguel-l-b <miguellopesbraido@gmail.com>
2023-10-29 17:27:25 -0300

    back ground color

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+/* eslint-disable react/jsx-pascal-case */
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import Students from './pages/Students'
 import Header from './components/Header'
 import Courses from './pages/Courses'
+import Carometer from './pages/Carometer'
 
 function App() {
-  const [path, setPath] = useState(window.location.pathname)
-  useEffect(() => setPath(window.location.pathname))
   return (
     <BrowserRouter>
       <Header.root>
         <div className="flex-col h-1/6 justify-around items-center sm:h-fit sm:flex-row flex mt-10 sm:mt-0 sm:w-1/6">
           <a href="https://cotuca.unicamp.br"><img src="/cotuca.png" className="h-10 w-10" alt="logo cotuca" /></a>
-          { path != "/" && <Header.button path="/" text="home" /> }
+          <Header.button path="/" text="home" />
         </div>
         <div className="sm:w-1/3 h-1/3 flex-col justify-around items-center sm:h-fit sm:flex-row flex sm:justify-between sm:items-center">
           <Header.button path="/student" text="aluno" />
@@ -26,7 +26,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/student" element={<Students />} />
           <Route path="/courses" element={<Courses />} />
-          <Route path="/carometer" element={<Students />} />
+          <Route path="/carometer" element={<Carometer />} />
         </Routes>
       </main>
     </BrowserRouter>
